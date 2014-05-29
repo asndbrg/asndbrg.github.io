be74c8fa9daf6e9a0141dd922923d913d3d8331b
Fredrik Wallgren <fredrik.wallgren@gmail.com>
2014-05-29 16:29:34 +0200

    New layout

--- javascripts/application.js
+++ javascripts/application.js
@@ -4,7 +4,7 @@
   document.oncontextmenu = context_menu;
 
   function context_menu(e) {
-  if (!e) var e = window.event;
+    if (!e) var e = window.event;
     var eTarget = (window.event) ? e.srcElement : e.target;
 
     if (eTarget.nodeName == "IMG") {
@@ -13,20 +13,4 @@
       return false;
     }
   }
-
-  window.onload = function () {
-    var menuButton = new MenuButton();
-  };
-
-  function MenuButton() {
-    this.id = 'menu-button';
-    this.wrapper = document.getElementById('wrapper');
-    this.button = document.getElementById(this.id);
-    this.button.onclick = this.toggleNav.bind(this);
-  }
-
-  MenuButton.prototype.toggleNav = function (event) {
-    event.preventDefault();
-    this.wrapper.classList.toggle('show-nav');
-  };
 })();
